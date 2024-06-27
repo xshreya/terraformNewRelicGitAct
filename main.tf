b5216085b955b8b91670e7292a2e42ec2c961b8d
Shreya Sharma <115712350+xshreya@users.noreply.github.com>
2024-06-27 13:29:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,11 @@
-#configure terraform block
 
-terraform{
-  required_providers{
-    newrelic = {
-      source = "newrelic/newrelic"
-      version = "3.3.0"
-    }
-  }
-}
-
-#configure new relic provider
-provider "newrelic" {
-  account_id = 4438275   # Your New Relic account ID
-  api_key = var.newrelic_api_key # Your New Relic user key
-  region = "US"        # US or EU (defaults to US)
-}
-
-module "alert_policy" {
-  source = "./alert_policy"
+module "alertPolicy" {
+  source = "./alertPolicy"
   incident_preference = "PER_POLICY"
 }
 
 
 module "newrelic_nrql_alert_condition" {
-  source = "./newrelic_nrql_alert_condition"
+  source = "./newRelicAlertCondition"
   policy_id  = module.alert_policy.policy
 }
